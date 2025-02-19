eb91b148ab4a5405b31957b11e07c5d28ead9f73
taijamartinez <taijasmartinez@gmail.com>
2025-02-18 21:40:48 -0500

    added nav.tsx code and added styling in index.css to make the links side by side.

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
-  )
+    <nav className="nav">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/SavedCandidates">Potential Candidates</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
+
+
+
